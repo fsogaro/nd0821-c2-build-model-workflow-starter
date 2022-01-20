3862f4cd2ecbabfb94aa83e851348752647c7772
fsogaro <fm.sogaro@gmail.com>
2022-01-20 11:48:00 +0100

    best parameters in config

--- main.py
+++ main.py
@@ -116,15 +116,19 @@ def go(config: DictConfig):
                 },
             )
 
-            pass
 
         if "test_regression_model" in active_steps:
+            _ = mlflow.run(
+                os.path.join(
+                    f"{config['main']['components_repository']}/test_regression_model"
+                ),
+                "main",
+                parameters={
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest",
+                },
+            )
 
-            ##################
-            # Implement here #
-            ##################
-
-            pass
 
 
 if __name__ == "__main__":
